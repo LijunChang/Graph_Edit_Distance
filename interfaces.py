82723a8a503aceb20841c9d9ddcdc436bdb5a37c
weathon <wguo6358@gmail.com>
2022-08-11 15:04:53 -0500

    -

--- interfaces.py
+++ interfaces.py
@@ -3,10 +3,10 @@ import os
 os.chdir('/home/wg25r/pyCMT/GED/RealGED')
 
 print(int(subprocess.check_output(
-    ['./a.out', '-d', '../dataForReal/15186818.txt', '-q', '../dataForReal/15922252.txt', '-g'])) == 6)
+    ['./old.out', '-d', '../dataForReal/15186818.txt', '-q', '../dataForReal/15922252.txt', '-g'])) == 6)
 
 
 def getGED(id1, id2):
     file1 = "../dataForReal/{}".format(id1)
     file2 = "../dataForReal/{}".format(id2)
-    return int(subprocess.check_output(['./a.out', '-d', file1, '-q', file2, '-g']))
+    return int(subprocess.check_output(['./old.out', '-d', file1, '-q', file2, '-g']))
